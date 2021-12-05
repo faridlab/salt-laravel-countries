344f46563af6cfe4e6b27dfc6d73808dbd961c8c
Farid Hidayat <e.faridhidayat@gmail.com>
2021-12-05 22:25:36 +0700

    Indonesia Country seeds

--- database/seeders/CountryIndonesiaSeeder.php
+++ database/seeders/CountryIndonesiaSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class SqlIndonesiaSeeder extends Seeder
+class CountryIndonesiaSeeder extends Seeder
 {
     /**
      * Run the database seeds.
