1a1441ba5bb00f2f26739ee1c48073a0654ad01a
Farid Hidayat <e.faridhidayat@gmail.com>
2022-01-16 23:50:24 +0700

    feat: /api/v1/provinces route

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::namespace('SaltCountries\Controllers')
   ->prefix("api/{$version}")
   ->group(function () {
 
-  // API: COUNNTRIES RESOURCES
+  // API: COUNTRIES RESOURCES
   Route::get("countries", 'ApiCountriesResourcesController@index'); // get entire collection
   Route::post("countries", 'ApiCountriesResourcesController@store'); // create new collection
 
@@ -32,5 +32,30 @@ Route::namespace('SaltCountries\Controllers')
   Route::patch("countries/{id}", 'ApiCountriesResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
   // DESTROY data by ID (id), selected IDs (selected), and All data (all)
   Route::delete("countries/{id}", 'ApiCountriesResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
+
+
+  // API: PROVINCES RESOURCES
+  Route::get("provinces", 'ApiCountriesResourcesController@index'); // get entire collection
+  Route::post("provinces", 'ApiCountriesResourcesController@store'); // create new collection
+
+  Route::get("provinces/trash", 'ApiCountriesResourcesController@trash'); // trash of collection
+
+  Route::post("provinces/import", 'ApiCountriesResourcesController@import'); // import collection from external
+  Route::post("provinces/export", 'ApiCountriesResourcesController@export'); // export entire collection
+  Route::get("provinces/report", 'ApiCountriesResourcesController@report'); // report collection
+
+  Route::get("provinces/{id}/trashed", 'ApiCountriesResourcesController@trashed')->where('id', '[a-zA-Z0-9]+'); // get collection by ID from trash
+
+  // RESTORE data by ID (id), selected IDs (selected), and All data (all)
+  Route::post("provinces/{id}/restore", 'ApiCountriesResourcesController@restore')->where('id', '[a-zA-Z0-9]+'); // restore collection by ID
+
+  // DELETE data by ID (id), selected IDs (selected), and All data (all)
+  Route::delete("provinces/{id}/delete", 'ApiCountriesResourcesController@delete')->where('id', '[a-zA-Z0-9]+'); // hard delete collection by ID
+
+  Route::get("provinces/{id}", 'ApiCountriesResourcesController@show')->where('id', '[a-zA-Z0-9]+'); // get collection by ID
+  Route::put("provinces/{id}", 'ApiCountriesResourcesController@update')->where('id', '[a-zA-Z0-9]+'); // update collection by ID
+  Route::patch("provinces/{id}", 'ApiCountriesResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
+  // DESTROY data by ID (id), selected IDs (selected), and All data (all)
+  Route::delete("provinces/{id}", 'ApiCountriesResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
 
 });
