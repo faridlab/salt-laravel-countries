248a30d4ddba7b24f71f4fa2269d2d7346239080
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-16 21:59:44 +0700

    chore: seperated files of indonesia data

---
+++ database/seeders/countries.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Records of countries
+-- ----------------------------
+BEGIN;
+INSERT INTO `countries` VALUES ('7ce4541d-04af-11ed-8fe4-0242ac1e0003', 'ID', 'Indonesia', 62, NULL, NULL, NULL);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
