3137d775582600a3cab198e90e5646489535d33f
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-16 22:00:45 +0700

    fix: seeder file one by one

--- database/seeders/CountryIndonesiaSeeder.php
+++ database/seeders/CountryIndonesiaSeeder.php
@@ -14,8 +14,28 @@ class CountryIndonesiaSeeder extends Seeder
      */
     public function run()
     {
-        $countries = base_path('database/seeders/indonesia.sql');
+        $countries = base_path('database/seeders/countries.sql');
         $sql = file_get_contents($countries);
+        DB::unprepared($sql);
+
+        $provinces = base_path('database/seeders/provinces.sql');
+        $sql = file_get_contents($provinces);
+        DB::unprepared($sql);
+
+        $cities = base_path('database/seeders/cities.sql');
+        $sql = file_get_contents($cities);
+        DB::unprepared($sql);
+
+        $districts = base_path('database/seeders/districts.sql');
+        $sql = file_get_contents($districts);
+        DB::unprepared($sql);
+
+        $subdistricts = base_path('database/seeders/subdistricts.sql');
+        $sql = file_get_contents($subdistricts);
+        DB::unprepared($sql);
+
+        $postalcode = base_path('database/seeders/postalcode.sql');
+        $sql = file_get_contents($postalcode);
         DB::unprepared($sql);
     }
 }
